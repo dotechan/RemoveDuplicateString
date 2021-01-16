541b8d62c63df29708e4d8bd86c86faa5519456c
dotechan <this.is.itoooo.0120@gmail.com>
2021-01-16 14:48:29 +0900

    Prettierによるformatをかけたことでインデントが空白スペース2個になった

--- remove_duplicate_string.js
+++ remove_duplicate_string.js
@@ -1,10 +1,10 @@
 /**
  * Usage:
  *      node remove_duplicate_string.js
- * 
- * Overview: 
+ *
+ * Overview:
  *      Androidのstring.xmlから重複定義されている用語を削除する
- * 
+ *
  * Description:
  *      ./srcディレクトリ配下に各言語のvalues-xxxファイルを配置する
  *      ./src/values-xxx/配下のstring-xxx.xmlファイルの中身を検証する
@@ -13,46 +13,55 @@
  *      すでに./destディレクトリが存在する場合はエラーになる
  */
 
-const dom = require('xmldom').DOMParser;
-const fs = require('fs');
-const SRC_DIR_PATH = './src/';
-const DEST_DIR_PATH = './dest/';
+const dom = require("xmldom").DOMParser;
+const fs = require("fs");
+const SRC_DIR_PATH = "./src/";
+const DEST_DIR_PATH = "./dest/";
 
 // 加工後のxmlファイルを生成して格納するディレクトリを生成する
 fs.mkdirSync(DEST_DIR_PATH);
 
-fs.readdirSync(SRC_DIR_PATH).forEach(valuesDir => {
-    fs.readdirSync(SRC_DIR_PATH + valuesDir).forEach(fileName => {
-        console.log(`start copying ${fileName}`);
+fs.readdirSync(SRC_DIR_PATH).forEach((valuesDir) => {
+  fs.readdirSync(SRC_DIR_PATH + valuesDir).forEach((fileName) => {
+    console.log(`start copying ${fileName}`);
 
-        const SRC_XML_FILE_PATH = SRC_DIR_PATH + valuesDir + '/';
-        const xmlFile = fs.readFileSync(SRC_XML_FILE_PATH + fileName, { encoding: 'utf8', mode: 'w+' });
+    const SRC_XML_FILE_PATH = SRC_DIR_PATH + valuesDir + "/";
+    const xmlFile = fs.readFileSync(SRC_XML_FILE_PATH + fileName, {
+      encoding: "utf8",
+      mode: "w+",
+    });
 
-        const document = new dom().parseFromString(xmlFile);
-        // NodeListがライブ(DOM内の更新が自動的にコレクションに反映される)か
-        // 静的(DOM内の変更がコレクションの内容に影響を与えない場合)の二種類がある
-        // getElementsByTagNameはライブであるNodeListを返す
-        const nodeList = document.getElementsByTagName('string');
-        let preAttributeValue = "";
-        // XML内のAttributeNodeのValueが重複しているElementを最初のElement以外削除する
-        // 前提条件:XML内のElementはname属性の値でソートされている
-        Array.from(nodeList).forEach(node => {
-            const attributeValue = node.getAttribute('name').nodeValue;
-            if (attributeValue == preAttributeValue) {
-                const oldChild = node.parentNode.removeChild(node);
-                console.log(`remove element, elementNodeName = ${oldChild.nodeName}, attributeNodeValue = ${attributeValue}, textNodeValue = ${oldChild.firstChild.nodeValue}`);
-            }
-            preAttributeValue = attributeValue;
-        })
+    const document = new dom().parseFromString(xmlFile);
+    // NodeListがライブ(DOM内の更新が自動的にコレクションに反映される)か
+    // 静的(DOM内の変更がコレクションの内容に影響を与えない場合)の二種類がある
+    // getElementsByTagNameはライブであるNodeListを返す
+    const nodeList = document.getElementsByTagName("string");
+    let preAttributeValue = "";
+    // XML内のAttributeNodeのValueが重複しているElementを最初のElement以外削除する
+    // 前提条件:XML内のElementはname属性の値でソートされている
+    Array.from(nodeList).forEach((node) => {
+      const attributeValue = node.getAttribute("name").nodeValue;
+      if (attributeValue == preAttributeValue) {
+        const oldChild = node.parentNode.removeChild(node);
+        console.log(
+          `remove element, elementNodeName = ${oldChild.nodeName}, attributeNodeValue = ${attributeValue}, textNodeValue = ${oldChild.firstChild.nodeValue}`
+        );
+      }
+      preAttributeValue = attributeValue;
+    });
 
-        // DOMを編集しただけではXMLファイルに反映されないのでDOMから文字列にシリアライズして、文字列からファイルを作成する
-        const XMLSerializer = require('xmldom').XMLSerializer;
-        const xmlStr = new XMLSerializer().serializeToString(document);
-        const DEST_XML_FILE_PATH = DEST_DIR_PATH + valuesDir + '/';
-        fs.mkdirSync(DEST_XML_FILE_PATH);
-        fs.writeFileSync(DEST_XML_FILE_PATH + fileName, xmlStr, { encoding: 'utf8', mode: 0o666, flag: 'w' });
+    // DOMを編集しただけではXMLファイルに反映されないのでDOMから文字列にシリアライズして、文字列からファイルを作成する
+    const XMLSerializer = require("xmldom").XMLSerializer;
+    const xmlStr = new XMLSerializer().serializeToString(document);
+    const DEST_XML_FILE_PATH = DEST_DIR_PATH + valuesDir + "/";
+    fs.mkdirSync(DEST_XML_FILE_PATH);
+    fs.writeFileSync(DEST_XML_FILE_PATH + fileName, xmlStr, {
+      encoding: "utf8",
+      mode: 0o666,
+      flag: "w",
+    });
 
-        console.log(`finish copying ${fileName}`);
-    })
-    console.log('\n');
+    console.log(`finish copying ${fileName}`);
+  });
+  console.log("\n");
 });
